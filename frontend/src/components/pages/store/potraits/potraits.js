17ac122451dba6203db88cfc7cdd153b06bfda6c
Ankush <ankushsharma9685@gmail.com>
2024-02-22 11:51:00 +0530

    added loader to handle the fetch time

--- frontend/src/components/pages/store/potraits/potraits.js
+++ frontend/src/components/pages/store/potraits/potraits.js
@@ -3,20 +3,21 @@ import Card from "../../../ui/card";
 
 const Potraits = () => {
     const [potratis, setPotraits] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     const fetchAllPotratis = async () => {
+        setIsLoading(true);
         const response = await fetch("https://fakestoreapi.com/products/category/jewelery");
         const potrait = await response.json();
         setPotraits((prev) => {
             return potrait
         })
-        console.log(potrait);
+        setIsLoading(false);
     }
 
     useEffect(() => {
         fetchAllPotratis();
     }, [])
-    console.log(potratis);
 
     return (
         <section id="potraits" className="w-full py-8 md:py-16 lg:py-24 border-t">
@@ -25,7 +26,9 @@ const Potraits = () => {
                     <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">Potraits</h2>
                 </div>
                 <ul className="mx-auto grid max-w-5xl items-start gap-6 sm:grid-cols-3 lg:gap-12">
-                    {potratis?.map(prod => <Card title={prod.title} price={prod.price} url={prod.image} />)}
+                    {!isLoading && potratis?.map(prod => <Card title={prod.title} price={prod.price} url={prod.image} />)}
+                    {!isLoading && potratis.length <= 0 && <p>no potarits found</p>}
+                    {isLoading && "Loading..."}
                 </ul>
             </div>
         </section>
