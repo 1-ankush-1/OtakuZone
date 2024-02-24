7f9c7254947ef4a219aad183da240b52074767ce
Ankush <ankushsharma9685@gmail.com>
2024-02-24 18:00:18 +0530

    added service to handel login

--- frontend/src/components/pages/auth/login.js
+++ frontend/src/components/pages/auth/login.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import AuthServices from '../../../services/authService';
 
 const Login = () => {
+    const [isSending, setIsSending] = useState(false);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -13,10 +15,25 @@ const Login = () => {
         setPassword(e.target.value);
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log('Email:', email);
-        console.log('Password:', password);
+        if (email === "" || password === "") {
+            alert("empty credentials")
+            return;
+        }
+
+        const response = await AuthServices.login({
+            email, password
+        })
+
+        setIsSending(true);
+        if (response.data !== null) {
+            console.log(response.data.idToken)
+            alert("successful login");
+        } else {
+            alert(response.error);
+        }
+        setIsSending(false);
     };
 
     return (
@@ -24,7 +41,7 @@ const Login = () => {
             <form onSubmit={handleSubmit} className="w-64">
                 <input type="email" placeholder="Email" value={email} onChange={handleEmailChange} className="mb-4 p-2 border border-gray-300 rounded" />
                 <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange} className="mb-4 p-2 border border-gray-300 rounded" />
-                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{isSending ? "logging..." : "Submit"}</button>
             </form>
             <div className="mt-4">
                 <Link to="../forget-password" className="text-blue-500 hover:text-blue-700">Forgot Password</Link>
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -23,8 +23,36 @@ const signupUser = async (user) => {
     }
 };
 
+const loginUser = async (user) => {
+    const url  = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDxuFYGPKR6ZSLAf52T2gImIGDfaFrigJU"
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 
+                ...user, 
+                returnSecureToken: true,
+             })
+        });
+
+        if (!response.ok) {
+            const errorResponse = await response.json();
+            throw new Error(errorResponse.error.message);
+        }
+
+        const data = await response.json();
+        return { data, error: null };
+    } catch (error) {
+        console.error('An error occurred in logging in user');
+        return { data: null, error: error.message };
+    }
+};
+
 const AuthServices = {
-    signup: signupUser
+    signup: signupUser,
+    login: loginUser
 };
 
 export default AuthServices;
