29b9105847e373af817f66f8f27733795bd9f218
Ankush <ankushsharma9685@gmail.com>
2024-03-02 16:47:36 +0530

    also storing email in localhost

--- frontend/src/stores/authContext.js
+++ frontend/src/stores/authContext.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 export const AuthContext = React.createContext({
     token: "",
@@ -13,15 +13,15 @@ const AuthContextProvider = (props) => {
     const initialState = localStorage.getItem("token");
     const intialEmail = localStorage.getItem("email");
 
-    const [email, setEmail] = useState(intialEmail);
+    const emailRef = useRef(intialEmail);
     const [token, setToken] = useState(initialState);
     const UserIsLoggedIn = !!token
 
     const handleAddToken = (token, email) => {
         setToken(token);
-        setToken(email);
+        emailRef.current = email;
         localStorage.setItem("token", token)
-        localStorage.setItem("email", email)
+        localStorage.setItem("email", emailRef.current)
     }
 
     const handleResetToken = (token) => {
@@ -32,15 +32,15 @@ const AuthContextProvider = (props) => {
     const handleRemoveToken = () => {
         setToken(null);
         localStorage.removeItem("token");
-        setEmail(null);
+        emailRef.current = null;
         localStorage.removeItem("email");
     };
 
-    //autolog out after 5 min
+    //autolog out after 30 min
     useEffect(() => {
         const timeout = setTimeout(() => {
             handleRemoveToken();
-        }, 5 * 60 * 1000);
+        }, 30 * 60 * 1000);
 
         return () => {
             clearTimeout(timeout);
@@ -48,7 +48,7 @@ const AuthContextProvider = (props) => {
     }, [token]);
 
     return (
-        <AuthContext.Provider value={{ email, token, isLoggedIn: UserIsLoggedIn, onLogin: handleAddToken, onLogout: handleRemoveToken, onReset: handleResetToken }}>
+        <AuthContext.Provider value={{ email:emailRef.current, token, isLoggedIn: UserIsLoggedIn, onLogin: handleAddToken, onLogout: handleRemoveToken, onReset: handleResetToken }}>
             {props.children}
         </AuthContext.Provider >
     )
